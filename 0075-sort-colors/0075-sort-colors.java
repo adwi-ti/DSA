e762c5b5a541f6b9adff115ec20e93c9a89bad68
adwi-ti <143506222+adwi-ti@users.noreply.github.com>
2026-08-03 12:44:20 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,29 @@
 class Solution {
+    public void swap(int[] arr ,int a , int b){
+        int temp = arr[a] ; 
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
     public void sortColors(int[] nums) {
-        int l = 0; 
-        int r = nums.length -1;
         int i = 0;
+        int l = 0 ;
+        int r = nums.length -1;
+
+
         while(i <= r){
             if(nums[i] == 0){
-                //swap nums[l]
-                int temp = nums[i];
-                nums[i] = nums[l];
-                nums[l] = temp;
-                l++;
+                swap(nums, i, l);
                 i++;
+                l++;
             }
+
             else if(nums[i] == 2){
-                int temp2 = nums[i];
-                nums[i] = nums[r];
-                nums[r] = temp2;
+                swap(nums, i , r);
                 r--;
             }
-            else{
-                i++;
-                }
 
+            else i++;
         }
-
-        
     }
 }
