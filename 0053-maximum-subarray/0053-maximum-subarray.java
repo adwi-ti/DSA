54836f579d50a3dd3bd223957aba704ed47a5443
adwi-ti <143506222+adwi-ti@users.noreply.github.com>
2026-08-03 12:25:16 +0530

    Time: 1 ms (99.95%), Space: 77.4 MB (32.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,19 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-         int ms = Integer.MIN_VALUE;
-        int n = nums.length;
-        int sum = 0;
-        int flag = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] > 0 ) flag++;
-            sum += nums[i];
-            if(sum < 0) sum = 0;
-            ms = Math.max(sum, ms);
+        int cs = 0 ;
+        int ms = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < nums.length; i++){
+            cs += nums[i];
+            ms = Math.max(cs, ms);
+            if(cs < 0 ) cs = 0;
         }
-        if(flag ==0){
-            Arrays.sort(nums);
-            ms = nums[n-1];
-        }
+
         return ms;
     }
 }
